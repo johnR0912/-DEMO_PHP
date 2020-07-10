744bf473ea145e98508f2cb8e1d8c7ba07402d20
johnR0912 <65005551+johnR0912@users.noreply.github.com>
2020-07-09 21:42:38 -0600

    Creacion del directorio ITCA y ficheros PHP

---
+++ ITCA/john.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "Hola yo me llamo john ali rodriguez ramirez";
+
+echo "Este es el contenido del archivo.php";
+
+?>
---
+++ ITCA/rodriguez.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "Este es mi primer trabajo en git bash";
+
+echo "Este es el contenido del archivo.php";
+
+?>
